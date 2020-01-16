7131f587473f14dfd3afec338ad71f97997476a4
yaolongtao <yaolongtao@shinow.com>
2020-01-16 09:45:05 +0800

    人工智能目录修改和添加内容

---
+++ YLT_Learn/算法与数据结构学习/面向对象/SimpleDemo.py
@@ -0,0 +1,7 @@
+# 字典的检索
+employee = {"EmployeID": "Employee Unique Identity!"}
+print(employee['EmployeID'])
+#模块导入访问
+import employee
+employee.EmployeID()
+print(employee.Age)
---
+++ YLT_Learn/算法与数据结构学习/面向对象/employee.py
@@ -0,0 +1,6 @@
+# this goes in employee.py
+def EmployeID():
+    print("Employee Unique Identity!")
+
+
+Age = "Employee age is 25"
